e1d3a50877a6130ed9c2c7b3680b2e4d412cbe00
zhangxq2019 <zhangxq0306@foxmail.com>
2020-09-26 16:03:27 +0800

    JigglyPuff-sing

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document.querySelector('.mouth').addEventListener('click', function () {
     document.querySelector('.mouth').classList.toggle('sing');
     document.querySelector('.music-note-one').classList.toggle('music-note');
     document.querySelector('.music-note-two').classList.toggle('music-note');
-    if(isPlaying){
+    if(isPlaying && !audio.paused){
         audio.pause()
         isPlaying = false
         // document.getElementsByTagName("body")[0].appendChild(audio);
